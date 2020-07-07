8bb17c2317e7b0315a90ab5d307383c9ef0d1558
Santos Frederic <frederic.santos@u-bordeaux.fr>
2020-07-07 15:56:38 +0200

    Fin session 1

--- 02_Demo_markup/auto/demo_markup.el
+++ 02_Demo_markup/auto/demo_markup.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -30,6 +30,23 @@
     "amssymb"
     "capt-of"
     "hyperref"
-    "color"))
+    "color"
+    "mathpazo")
+   (LaTeX-add-labels
+    "sec:orgdec5e37"
+    "sec:orgb9c7125"
+    "sec-dev"
+    "sec:org608310b"
+    "sec:org170acce"
+    "sec:org016725c"
+    "sec:org8adb8f2"
+    "sec:org7bf8ba8"
+    "sec:org106850d"
+    "sec:org71f7734"
+    "tab-concur"
+    "sec:orga147540"
+    "sec:org5e0747f"
+    "fig-capture"
+    "sec:org8f7fcfe"))
  :latex)
 
